b81d74202d47c75dec21fa859f2f5f6fe9345e90
sammiearchie77 <chinonsosamuel29@gmail.com>
2021-07-25 20:47:18 +0100

    refactored dashboard

--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -5,17 +5,12 @@ import ChartistGraph from "react-chartist";
 import { makeStyles } from "@material-ui/core/styles";
 import Icon from "@material-ui/core/Icon";
 // @material-ui/icons
-import Store from "@material-ui/icons/Store";
-import Warning from "@material-ui/icons/Warning";
-import DateRange from "@material-ui/icons/DateRange";
-import LocalOffer from "@material-ui/icons/LocalOffer";
-import Update from "@material-ui/icons/Update";
 import ArrowUpward from "@material-ui/icons/ArrowUpward";
 import AccessTime from "@material-ui/icons/AccessTime";
 import Accessibility from "@material-ui/icons/Accessibility";
 import BugReport from "@material-ui/icons/BugReport";
-import Code from "@material-ui/icons/Code";
-import Cloud from "@material-ui/icons/Cloud";
+import Info from "@material-ui/icons/Info";
+import Check from "@material-ui/icons/Check";
 // layout for this page
 import Admin from "layouts/Admin.js";
 // core components
@@ -31,7 +26,7 @@ import CardIcon from "components/Card/CardIcon.js";
 import CardBody from "components/Card/CardBody.js";
 import CardFooter from "components/Card/CardFooter.js";
 
-import { bugs, website, server } from "variables/general.js";
+import { bugs, pending, resolved } from "variables/general.js";
 
 import {
   dailySalesChart,
@@ -51,20 +46,20 @@ function Dashboard() {
           <Card>
             <CardHeader color="warning" stats icon>
               <CardIcon color="warning">
-                <Icon>content_copy</Icon>
+                <Icon> <Accessibility /> </Icon>
               </CardIcon>
-              <p className={classes.cardCategory}>Used Space</p>
+              <p className={classes.cardCategory}>Total Users</p>
               <h3 className={classes.cardTitle}>
-                49/50 <small>GB</small>
+                300
               </h3>
             </CardHeader>
             <CardFooter stats>
               <div className={classes.stats}>
                 <Danger>
-                  <Warning />
+                <Accessibility />
                 </Danger>
-                <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                  Get more space
+                <a href="#" onClick={(e) => e.preventDefault()}>
+                  View Users
                 </a>
               </div>
             </CardFooter>
@@ -74,15 +69,19 @@ function Dashboard() {
           <Card>
             <CardHeader color="dark" stats icon>
               <CardIcon color="dark">
-                <Store />
+              <Accessibility />
               </CardIcon>
-              <p className={classes.cardCategory}>Revenue</p>
-              <h3 className={classes.cardTitle}>$34,245</h3>
+              <p className={classes.cardCategory}>Total Art</p>
+              <h3 className={classes.cardTitle}>40</h3>
             </CardHeader>
             <CardFooter stats>
               <div className={classes.stats}>
-                <DateRange />
-                Last 24 Hours
+                <Danger>
+                <Accessibility />
+                </Danger>
+                <a href="#" onClick={(e) => e.preventDefault()}>
+                  View Artizans
+                </a>
               </div>
             </CardFooter>
           </Card>
@@ -91,15 +90,19 @@ function Dashboard() {
           <Card>
             <CardHeader color="danger" stats icon>
               <CardIcon color="danger">
-                <Icon>info_outline</Icon>
+                <Accessibility />
               </CardIcon>
-              <p className={classes.cardCategory}>Fixed Issues</p>
+              <p className={classes.cardCategory}>Active Users</p>
               <h3 className={classes.cardTitle}>75</h3>
             </CardHeader>
             <CardFooter stats>
               <div className={classes.stats}>
-                <LocalOffer />
-                Tracked from Github
+                <Danger>
+                <Accessibility />
+                </Danger>
+                <a href="#" onClick={(e) => e.preventDefault()}>
+                  View Active Users
+                </a>
               </div>
             </CardFooter>
           </Card>
@@ -110,13 +113,17 @@ function Dashboard() {
               <CardIcon color="info">
                 <Accessibility />
               </CardIcon>
-              <p className={classes.cardCategory}>Followers</p>
-              <h3 className={classes.cardTitle}>+245</h3>
+              <p className={classes.cardCategory}>Active Art</p>
+              <h3 className={classes.cardTitle}>45</h3>
             </CardHeader>
             <CardFooter stats>
               <div className={classes.stats}>
-                <Update />
-                Just Updated
+                <Danger>
+                <Accessibility />
+                </Danger>
+                <a href="#" onClick={(e) => e.preventDefault()}>
+                  View Active Artizans
+                </a>
               </div>
             </CardFooter>
           </Card>
@@ -135,19 +142,19 @@ function Dashboard() {
               />
             </CardHeader>
             <CardBody>
-              <h4 className={classes.cardTitle}>Daily Sales</h4>
+              <h4 className={classes.cardTitle}>Daily Reguest Artizans</h4>
               <p className={classes.cardCategory}>
                 <span className={classes.successText}>
-                  <ArrowUpward className={classes.upArrowCardCategory} /> 55%
+                  <ArrowUpward className={classes.upArrowCardCategory} /> 5%
                 </span>{" "}
-                increase in today sales.
+                increase today.
               </p>
             </CardBody>
             <CardFooter chart>
               <div className={classes.stats}>
                 <AccessTime /> updated 4 minutes ago
               </div>
-            </CardFooter>
+            </CardFooter> 
           </Card>
         </GridItem>
         <GridItem xs={12} sm={12} md={4}>
@@ -163,19 +170,24 @@ function Dashboard() {
               />
             </CardHeader>
             <CardBody>
-              <h4 className={classes.cardTitle}>Email Subscriptions</h4>
-              <p className={classes.cardCategory}>Last Campaign Performance</p>
+              <h4 className={classes.cardTitle}>Total Subscriptions</h4>
+              <p className={classes.cardCategory}>
+                <span className={classes.successText}>
+                  <ArrowUpward className={classes.upArrowCardCategory} /> 5%
+                </span>{" "}
+                  increase today.
+              </p>
             </CardBody>
             <CardFooter chart>
               <div className={classes.stats}>
-                <AccessTime /> campaign sent 2 days ago
+                <AccessTime /> updated 4 minutes ago
               </div>
             </CardFooter>
           </Card>
         </GridItem>
         <GridItem xs={12} sm={12} md={4}>
           <Card chart>
-            <CardHeader color="dark">
+            <CardHeader color="primary">
               <ChartistGraph
                 className="ct-chart"
                 data={completedTasksChart.data}
@@ -185,12 +197,17 @@ function Dashboard() {
               />
             </CardHeader>
             <CardBody>
-              <h4 className={classes.cardTitle}>Completed Tasks</h4>
-              <p className={classes.cardCategory}>Last Campaign Performance</p>
+              <h4 className={classes.cardTitle}> Daily Completed Tasks</h4>
+              <p className={classes.cardCategory}>
+                <span className={classes.successText}>
+                  <ArrowUpward className={classes.upArrowCardCategory} /> 5%
+                </span>{" "}
+                  increase today.
+              </p>
             </CardBody>
             <CardFooter chart>
               <div className={classes.stats}>
-                <AccessTime /> campaign sent 2 days ago
+                <AccessTime /> updated 4 minutes ago
               </div>
             </CardFooter>
           </Card>
@@ -214,24 +231,24 @@ function Dashboard() {
                 ),
               },
               {
-                tabName: "Website",
-                tabIcon: Code,
+                tabName: "Pending",
+                tabIcon: Info,
                 tabContent: (
                   <Tasks
                     checkedIndexes={[0]}
                     tasksIndexes={[0, 1]}
-                    tasks={website}
+                    tasks={pending}
                   />
                 ),
               },
               {
-                tabName: "Server",
-                tabIcon: Cloud,
+                tabName: "Resolved",
+                tabIcon: Check,
                 tabContent: (
                   <Tasks
                     checkedIndexes={[1]}
                     tasksIndexes={[0, 1, 2]}
-                    tasks={server}
+                    tasks={resolved}
                   />
                 ),
               },
@@ -241,20 +258,17 @@ function Dashboard() {
         <GridItem xs={12} sm={12} md={6}>
           <Card>
             <CardHeader color="warning">
-              <h4 className={classes.cardTitleWhite}>Employees Stats</h4>
-              <p className={classes.cardCategoryWhite}>
-                New employees on 15th September, 2016
-              </p>
+              <h4 className={classes.cardTitleWhite}>Artizan Users</h4> 
             </CardHeader>
             <CardBody>
               <Table
                 tableHeaderColor="warning"
-                tableHead={["ID", "Name", "Salary", "Country"]}
+                tableHead={["ID", "Name", "Email", "Phone Number"]}
                 tableData={[
-                  ["1", "Dakota Rice", "$36,738", "Niger"],
-                  ["2", "Minerva Hooper", "$23,789", "Curaçao"],
-                  ["3", "Sage Rodriguez", "$56,142", "Netherlands"],
-                  ["4", "Philip Chaney", "$38,735", "Korea, South"],
+                  ["1", "Dakota Rice", "dakotarice123@yahoo.com", "09021239832"],
+                  ["2", "Minerva Hooper", "minervahoooper@gmail.com", "08123458932"],
+                  ["3", "Sage Rodriguez", "sagerodriguez@hotmail.com", "07043245678"],
+                  ["4", "Philip Chaney", "philipchaney@gmail.com", "08119929772"],
                 ]}
               />
             </CardBody>
