8ebfe67f2e1987049f328524cee972b97b2d495d
Uka Emmanuel Ogbonna <ukaemmanuel200@gmail.com>
2021-07-25 21:05:40 +0100

    pulled sammy work

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -17,22 +17,23 @@ export default function Footer(props) {
         <div className={classes.left}>
           <List className={classes.list}>
             <ListItem className={classes.inlineBlock}>
-              <a href="#home" className={classes.block}>
+              <a href="/" className={classes.block}>
                 Home
+                
               </a>
             </ListItem>
             <ListItem className={classes.inlineBlock}>
-              <a href="#company" className={classes.block}>
+              <a href="/" className={classes.block}>
                 Company
               </a>
             </ListItem>
             <ListItem className={classes.inlineBlock}>
-              <a href="#portfolio" className={classes.block}>
+              <a href="/" className={classes.block}>
                 Portfolio
               </a>
             </ListItem>
             <ListItem className={classes.inlineBlock}>
-              <a href="#blog" className={classes.block}>
+              <a href="/" className={classes.block}>
                 Blog
               </a>
             </ListItem>
@@ -42,13 +43,12 @@ export default function Footer(props) {
           <span>
             &copy; {1900 + new Date().getYear()}{" "}
             <a
-              href="https://www.creative-tim.com?ref=njsmd-footer"
-              target="_blank"
+              href="/"
               className={classes.a}
             >
-              Creative Tim
+              Artizans 
             </a>
-            , made with love for a better web
+            , We give you what you want.
           </span>
         </p>
       </div>
