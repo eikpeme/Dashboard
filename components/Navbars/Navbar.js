3467990c63baa9fab37284ac491128ffb87494ef
Uka Emmanuel Ogbonna <ukaemmanuel200@gmail.com>
2021-07-26 23:39:38 +0100

    tyf

--- components/Navbars/Navbar.js
+++ components/Navbars/Navbar.js
@@ -47,7 +47,7 @@ export default function Header(props) {
           </Button>
         </div>
         <Hidden smDown implementation="css">
-          {props.rtlActive? null : <AdminNavbarLinks />  }
+          {props.rtlActive? null : <AdminNavbarLinks />}
         </Hidden>
         <Hidden mdUp implementation="css">
           <IconButton
