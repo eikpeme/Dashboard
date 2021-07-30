3ea71353404ea8cd4d7524ac05d836438fce72a2
Uka Emmanuel Ogbonna <ukaemmanuel200@gmail.com>
2021-07-30 04:08:14 +0100

    activate/deactivated

--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -44,7 +44,7 @@ function Dashboard() {
       <GridContainer>
         <GridItem xs={12} sm={6} md={3}>
           <Card>
-            <CardHeader color="warning" stats icon>
+            <CardHeader color="primary" stats icon>
               <CardIcon color="warning">
                 <Icon> <Accessibility /> </Icon>
               </CardIcon>
@@ -67,7 +67,7 @@ function Dashboard() {
         </GridItem>
         <GridItem xs={12} sm={6} md={3}>
           <Card>
-            <CardHeader color="dark" stats icon>
+            <CardHeader color="primary" stats icon>
               <CardIcon color="dark">
               <Accessibility />
               </CardIcon>
@@ -88,7 +88,7 @@ function Dashboard() {
         </GridItem>
         <GridItem xs={12} sm={6} md={3}>
           <Card>
-            <CardHeader color="danger" stats icon>
+            <CardHeader color="primary" stats icon>
               <CardIcon color="danger">
                 <Accessibility />
               </CardIcon>
@@ -109,7 +109,7 @@ function Dashboard() {
         </GridItem>
         <GridItem xs={12} sm={6} md={3}>
           <Card>
-            <CardHeader color="info" stats icon>
+            <CardHeader color="primary" stats icon>
               <CardIcon color="info">
                 <Accessibility />
               </CardIcon>
@@ -132,7 +132,7 @@ function Dashboard() {
       <GridContainer>
         <GridItem xs={12} sm={12} md={4}>
           <Card chart>
-            <CardHeader color="success">
+            <CardHeader color="primary">
               <ChartistGraph
                 className="ct-chart"
                 data={dailySalesChart.data}
@@ -159,7 +159,7 @@ function Dashboard() {
         </GridItem>
         <GridItem xs={12} sm={12} md={4}>
           <Card chart>
-            <CardHeader color="warning">
+            <CardHeader color="primary">
               <ChartistGraph
                 className="ct-chart"
                 data={emailsSubscriptionChart.data}
@@ -257,12 +257,12 @@ function Dashboard() {
         </GridItem>
         <GridItem xs={12} sm={12} md={6}>
           <Card>
-            <CardHeader color="warning">
+            <CardHeader color="primary">
               <h4 className={classes.cardTitleWhite}>Artizan Users</h4> 
             </CardHeader>
             <CardBody>
               <Table
-                tableHeaderColor="warning"
+                tableHeaderColor="primary"
                 tableHead={["ID", "Name", "Email", "Phone Number"]}
                 tableData={[
                   ["1", "Dakota Rice", "dakotarice123@yahoo.com", "09021239832"],
@@ -274,6 +274,90 @@ function Dashboard() {
             </CardBody>
           </Card>
         </GridItem>
+        <GridContainer>
+        <GridItem xs={12} sm={12} md={6}>
+          <CustomTabs
+            title="Users:"
+            headerColor="dark"
+            tabs={[
+              {
+                tabName: "Activate/Deactivate",
+                tabIcon: BugReport,
+                tabContent: (
+                  <Tasks
+                    checkedIndexes={[0, 3]}
+                    tasksIndexes={[0, 1, 2, 3]}
+                    tasks={bugs}
+                  />
+                ),
+              },
+              {
+                tabName: "Activated",
+                tabIcon: Info,
+                tabContent: (
+                  <Tasks
+                    checkedIndexes={[0]}
+                    tasksIndexes={[0, 1]}
+                    tasks={pending}
+                  />
+                ),
+              },
+              {
+                tabName: "Deactivated",
+                tabIcon: Check,
+                tabContent: (
+                  <Tasks
+                    checkedIndexes={[1]}
+                    tasksIndexes={[0, 1, 2]}
+                    tasks={resolved}
+                  />
+                ),
+              },
+            ]}
+          />
+        </GridItem>
+        <GridItem xs={12} sm={12} md={6}>
+          <CustomTabs
+            title="Artizans:"
+            headerColor="dark"
+            tabs={[
+              {
+                tabName: "Activate/Deactivate",
+                tabIcon: BugReport,
+                tabContent: (
+                  <Tasks
+                    checkedIndexes={[0, 3]}
+                    tasksIndexes={[0, 1, 2, 3]}
+                    tasks={bugs}
+                  />
+                ),
+              },
+              {
+                tabName: "Activated",
+                tabIcon: Info,
+                tabContent: (
+                  <Tasks
+                    checkedIndexes={[0]}
+                    tasksIndexes={[0, 1]}
+                    tasks={pending}
+                  />
+                ),
+              },
+              {
+                tabName: "Deactivated",
+                tabIcon: Check,
+                tabContent: (
+                  <Tasks
+                    checkedIndexes={[1]}
+                    tasksIndexes={[0, 1, 2]}
+                    tasks={resolved}
+                  />
+                ),
+              },
+            ]}
+          />
+        </GridItem>
+        </GridContainer>
       </GridContainer>
     </div>
   );
