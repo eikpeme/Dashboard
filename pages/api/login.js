1a8c357a9498e962587636b9115b6a9016c2959c
Uka Emmanuel Ogbonna <ukaemmanuel200@gmail.com>
2021-08-06 23:48:15 +0100

    authlogin

--- pages/api/login.js
+++ pages/api/login.js
@@ -9,17 +9,17 @@ export default async (req, res) => {
   };
 
   return axios
-    .post(`${process.env.PRIVATE_API_URL}/auth/admin/login`, loginParams)
+    .post('https://artizan-api-staged.herokuapp/auth/admin/login', loginParams )
     .then((response) => {
-      if (response.status === 'success') {
-        cookies.set('jwt', response.token, {
+      if (response.data.status === 'success') {
+        cookies.set('jwt', response.data.token, {
           httpOnly: true,
           expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 60 * 60 * 1000),
           sameSite: 'lax',
         });
 
         return res.status(200).json({
-          status: 'success',
+          status: 'success', 
         });
       } 
 
@@ -28,7 +28,7 @@ export default async (req, res) => {
     .catch((err) => {
       return res.status(401).json({
         status: 'fail',
-        message: err.response,
+        message: err.response.data.message ,
       });
     });
 };
